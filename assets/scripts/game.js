ed321660939e83797ac1c8b7b8892d97f1cc0686
lisamsmith100 <lisamarie.smith@libertymutual.com>
2017-04-05 16:52:21 -0400

    Update for authentication
    Troubleshooting change password still

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,4 +1,4 @@
-'use strict'
+/*'use strict'
 
 /*const boggleVariants = require('../lib/challenge.js')
 const boggle = boggleVariants.boggle
@@ -8,16 +8,3 @@ const superBigBoggle = boggleVariants.superBigBoggle
 const boggleTray = 'wxxxxoxxxxrxxxxd'.split('')
 
 console.log(boggle(boggleTray, [[0, 0], [1, 1], [2, 2], [3, 3]])) */
-
-let playerX = X
-let playerY = Y
-
-let cells": ["","","","","","","","",""]
-
-const = game {
-  id,
-  cells,
-  over,
-  playerId,
-  playerEmail
-}
--- scripts/change-password.sh
+++ scripts/change-password.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
-#curl "http://localhost:4741/change-password/${ID}" \
-curl "http://httpbin.org/patch?id=${ID}" \
-  --include \
-  --request PATCH \
-  --data-urlencode ""
-
-# data output from curl doesn't have a trailing newline
-echo
+#ID=15 OLDPW=pass NEWPW=password TOKEN=BAhJIiUxMGQyYjgyNTRiNjExNmUyMTQ3Zjk1MzA0ZWY5YWY2MAY6BkVG--aa3a4fb7406394ec35ec879450fc2a4c96b5188e sh scripts/change-password-json.sh
+API="${API_ORIGIN:-http://localhost:4741}"
+URL_PATH="/change-password/$ID"
+curl --include --request PATCH "${API}${URL_PATH}" \
+  --header "Authorization: Token token=$TOKEN" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "passwords": {
+      "old": "'"${OLDPW}"'",
+      "new": "'"${NEWPW}"'"
+    }
+  }'
