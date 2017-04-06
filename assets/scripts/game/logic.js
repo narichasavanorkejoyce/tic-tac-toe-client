8b835e49bb9e36daef1c2e5ce3bd975aff615473
lisamsmith100 <lisamarie.smith@libertymutual.com>
2017-04-06 11:25:07 -0400

    Add images and update to game logic
    After some git re-work, adding code and images.

--- assets/scripts/game/logic.js
+++ assets/scripts/game/logic.js
@@ -18,6 +18,23 @@ let gameStats = {
   totalWins
 }
 
+{
+  "game": {
+    "id": 1,
+    "cells": ["","","","","","","","",""],
+    "over":false,
+    "player_x": {
+      "id": 1,
+      "email": "and@and.com"
+      },
+    "player_o": {
+      "id": 3,
+      "email":
+      "dna@dna.com"
+    }
+  }
+}
+
 module.exports = {
   gameArray,
   player_x,
