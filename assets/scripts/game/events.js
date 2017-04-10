d49c4e933a6f037ed2d8958bf73799f2c5af9bad
lisamsmith100 <lisamarie.smith@libertymutual.com>
2017-04-09 21:03:14 -0400

    Add clear game code, working on one-click and end click after game won
    Additional work required, committing to save progress

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -4,9 +4,18 @@
 const api = require('./api')
 const ui = require('./ui')
 const store = require('../store')
+const blankBoardPiece0 = $('.0').clone()
+const blankBoardPiece1 = $('.1').clone()
+const blankBoardPiece2 = $('.2').clone()
+const blankBoardPiece3 = $('.3').clone()
+const blankBoardPiece4 = $('.4').clone()
+const blankBoardPiece5 = $('.5').clone()
+const blankBoardPiece6 = $('.6').clone()
+const blankBoardPiece7 = $('.7').clone()
+const blankBoardPiece8 = $('.8').clone()
 
 let currentPlayer = 'X'
-let gameBoard = ["", "", "", "", "", "", "", "", ""]
+let gameBoard = ['', '', '', '', '', '', '', '', '']
 
 const onCreateGame = function (event) {
   event.preventDefault()
@@ -16,6 +25,20 @@ const onCreateGame = function (event) {
   api.createGame(this.data)
     .then(ui.createGameSuccess)
     .catch(ui.createGameFailure)
+}
+
+const onClearBoard = function (event) {
+  event.preventDefault()
+  console.log('clearBoard worked')
+  $('.0').replaceWith(blankBoardPiece0.clone())
+  $('.1').replaceWith(blankBoardPiece1.clone())
+  $('.2').replaceWith(blankBoardPiece2.clone())
+  $('.3').replaceWith(blankBoardPiece3.clone())
+  $('.4').replaceWith(blankBoardPiece4.clone())
+  $('.5').replaceWith(blankBoardPiece5.clone())
+  $('.6').replaceWith(blankBoardPiece6.clone())
+  $('.7').replaceWith(blankBoardPiece7.clone())
+  $('.8').replaceWith(blankBoardPiece8.clone())
 }
 
 const onTotalGamesByUser = function (event) {
@@ -31,108 +54,110 @@ const onClickBoard = function (event) {
   const value = currentPlayer
 //  let store.game = data.game
   console.log('enter for loop next')
-    if (currentPlayer === 'X') {
-        const index = $(this).attr('class')
-        console.log('index is ', index)
+  if (currentPlayer === 'X') {
+    const index = $(this).attr('class')
+    console.log('index is ', index)
   //      let games[pieceID] = value
-        console.log('the value is ', value)
-        console.log('player is ', currentPlayer)
-        $(this).text('X')
-        gameBoard[index] = value
-        let over = isWin(gameBoard, value)
-        const data = {
-          'game': {
-            'cell': {
-              'index': +index,
-              'value': 'x'
-            },
-            'over': over
-          }
-        }
-        api.updateGame(data)
-          .then(ui.updateGameSuccess)
-        console.log('just played a(n) ', currentPlayer, ' in index ', index)
-        currentPlayer = 'O'
-      } else {
-        const index = $(this).attr('class')
-        console.log('index is ', index)
-        console.log('the value is ', value)
-        console.log('player is ', currentPlayer)
-        $(this).text('O')
-        gameBoard[index] = value
-        let over = isWin(gameBoard, value)
-        const data = {
-          'game': {
-            'cell': {
-              'index': +index,
-              'value': 'o'
-            },
-            'over': false
-          }
-        }
-        api.updateGame(data)
-          .then(ui.updateGameSuccess)
-        console.log('just played a(n) ', currentPlayer, ' in index ', index)
-        currentPlayer = 'X'
+    console.log('the value is ', value)
+    console.log('player is ', currentPlayer)
+    $(this).text('X')
+    gameBoard[index] = value
+    let over = isWin(gameBoard, value)
+    const data = {
+      'game': {
+        'cell': {
+          'index': +index,
+          'value': 'x'
+        },
+        'over': over
       }
-      return currentPlayer
     }
+    api.updateGame(data)
+      .then(ui.updateGameSuccess)
+    console.log('just played a(n) ', currentPlayer, ' in index ', index)
+    currentPlayer = 'O'
+  } else {
+    const index = $(this).attr('class')
+    console.log('index is ', index)
+    console.log('the value is ', value)
+    console.log('player is ', currentPlayer)
+    $(this).text('O')
+    gameBoard[index] = value
+    let over = isWin(gameBoard, value)
+    const data = {
+      'game': {
+        'cell': {
+          'index': +index,
+          'value': 'o'
+        },
+        'over': false
+      }
+    }
+    api.updateGame(data)
+      .then(ui.updateGameSuccess)
+    console.log('just played a(n) ', currentPlayer, ' in index ', index)
+    currentPlayer = 'X'
+  }
+  return currentPlayer
+}
 
 const isWin = function (board, currentPlayer) {
   const gameBoard = board
   const player = currentPlayer
-  if (
-     // First row check
-     ((gameBoard[0] === player) && (gameBoard[1] === player) && (gameBoard[2] === player)) ||
-     // example: ["X"," "," ","X"," "," ","X"," "," "]
-     // Second row check
-     ((gameBoard[3] === player) && (gameBoard[4] === player) && (gameBoard[5] === player)) ||
-      // Third row check
-     ((gameBoard[6] === player) && (gameBoard[7] === player) && (gameBoard[8] === player)) ||
-     // First column check
-     ((gameBoard[0] === player) && (gameBoard[3] === player) && (gameBoard[6] === player)) ||
-     // Second column check
-     ((gameBoard[1] === player) && (gameBoard[4] === player) && (gameBoard[7] === player)) ||
-      // Third column check
-     ((gameBoard[2] === player) && (gameBoard[5] === player) && (gameBoard[8] === player)) ||
-     // Downward Diag check
-     ((gameBoard[0] === player) && (gameBoard[4] === player) && (gameBoard[8] === player)) ||
-      // Upward Diag check
-     ((gameBoard[6] === player) && (gameBoard[4] === player) && (gameBoard[2] === player))) {
+  let over = false
+  if ((gameBoard[0] === gameBoard[1]) && (gameBoard[1] === gameBoard[2]) && (gameBoard[2] === player)) {
+    console.log('player ', player, ' won') // 1st row win
+    $('p').text('Player ', player, ' won!')
     over = true
     return over
-  } else if { // draw scenario in here -
-    ((gameBoard[0] !== '&nbsp') && (gameBoard[1] !== '&nbsp') && (gameBoard[2] !== '&nbsp') &&
-      (gameBoard[3] !== '&nbsp') && (gameBoard[4] !== '&nbsp') && (gameBoard[5] !== '&nbsp') &&
-      (gameBoard[6] !== '&nbsp') && (gameBoard[7] !== ''&nbsp') && (gameBoard[8] !== '&nbsp')
-      over = true
-      return over
+  } else if ((gameBoard[3] === gameBoard[4]) && (gameBoard[4] === gameBoard[5]) && (gameBoard[5] === player)) {
+    console.log('player ', player, ' won') // 2nd row win
+    $('.displayWinner').text('Player ', player, ' won!')
+    over = true
+    return over
+  } else if ((gameBoard[6] === gameBoard[7]) && (gameBoard[7] === gameBoard[8]) && (gameBoard[8] === player)) {
+    console.log('player ', player, ' won') // 3rd row win
+    $('.displayWinner').text('Player ', player, ' won!')
+    over = true
+    return over
+  } else if ((gameBoard[0] === gameBoard[3]) && (gameBoard[3] === gameBoard[6]) && (gameBoard[6] === player)) {
+    console.log('player ', player, ' won') // 1st column win
+    $('.displayWinner').text('Player ', player, ' won!')
+    over = true
+    return over
+  } else if ((gameBoard[1] === gameBoard[4]) && (gameBoard[4] === gameBoard[7]) && (gameBoard[7] === player)) {
+    console.log('player ', player, ' won') // 2nd column win
+    $('.displayWinner').text('Player ', player, ' won!')
+    over = true
+    return over
+  } else if ((gameBoard[2] === gameBoard[5]) && (gameBoard[5] === gameBoard[8]) && (gameBoard[8] === player)) {
+    console.log('player ', player, ' won') // 3rd column win
+    $('.displayWinner').text('Player ', player, ' won!')
+    over = true
+    return over
+  } else if ((gameBoard[0] === gameBoard[4]) && (gameBoard[4] === gameBoard[8]) && (gameBoard[8] === player)) {
+    console.log('player ', player, ' won') // L to R diagonal win
+    $('.displayWinner').text('Player ', player, ' won!')
+    over = true
+    return over
+  } else if ((gameBoard[2] === gameBoard[4]) && (gameBoard[4] === gameBoard[6]) && (gameBoard[6] === player)) {
+    console.log('player ', player, ' won') // R to L diagonal win
+    $('.displayWinner').text('Player ', player, ' won!')
+    over = true
+    return over
+  } else if ((gameBoard[0] === ('X' || 'O')) && (gameBoard[1] === ('X' || 'O')) && (gameBoard[2] === ('X' || 'O')) && (gameBoard[3] === ('X' || 'O')) && (gameBoard[4] === ('X' || 'O')) && (gameBoard[5] === ('X' || 'O')) && (gameBoard[6] === ('X' || 'O')) && (gameBoard[7] === ('X' || 'O'))) {
+    $('.displayWinner').text('Nobody won! Ending is a cat like Meow!')
+    let over = true // draw
+    return over
   } else {
-    over = false
+    let over = false
   }
   return over
 }
-//   let boardpieceID = $(this).attr('class')
-//   currentPlayer = 'O'
-//   $(this).text(currentPlayer)
-//   boardpieceID = $(this).attr('class')
-//   console.log('just played a(n) ', currentPlayer, ' in boardpieceID ', boardpieceID)
-//   return currentPlayer
-//
-
-//
-// const clearBoard = function (game) {
-//   console.log('Clearboard ran!')
-//   const board = game
-//   for (let i = 0; i < 9; i++) {
-//     boardpieceID = $(this).attr('class')
-//     board[boardpieceID] = $(this).text('')
-//   }
-//   return board
-// }
 
 const addHandlers = () => {
   $('#createGame').on('click', onCreateGame)
+  $('#clearBoard').on('click', onClearBoard)
   $('.0').on('click', onClickBoard)
   $('.1').on('click', onClickBoard)
   $('.2').on('click', onClickBoard)
@@ -148,5 +173,6 @@ const addHandlers = () => {
 module.exports = {
   addHandlers,
   onClickBoard,
+  onClearBoard,
   onTotalGamesByUser
 }
